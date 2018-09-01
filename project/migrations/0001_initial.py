9a5d14c21b1470c1c60632b9c7350ec581e3faf8
Younes JAAIDI <yjaaidi@gmail.com>
2018-09-01 16:38:14 +0200

    Added db migrations.

---
+++ project/migrations/0001_initial.py
@@ -0,0 +1,18 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Member',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+                ('first_name', models.CharField(max_length=128)),
+                ('last_name', models.CharField(max_length=128)),
+            ],
+        ),
+    ]
